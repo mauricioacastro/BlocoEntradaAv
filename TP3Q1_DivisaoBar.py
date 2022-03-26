c375a2f3bd8e743485651a15039e5722d983e28c
mauricioacastro <mauricio.castro@al.infnet.edu.br>
2022-03-26 11:12:59 -0300

    TP3Q1_DivisaoBar_Parcial_2

--- TP3Q1_DivisaoBar.py
+++ TP3Q1_DivisaoBar.py
@@ -1,6 +1,6 @@
 # Função : Calcule a divisão de uma conta de bar 
 # Autor : Maurício Castro
-# Data : 25/03/2022
+# Data : 26/03/2022
 
 #Fluxo de exceção:
 #O programa deve verificar se o número total de pessoas é maior do que zero.
@@ -10,11 +10,12 @@
 
 totalConsumo = 0
 totalConta = 0
+contaPessoa = 0
 pessoas = 0
 servico = 0
 
 totalConsumo = str(input("Informe o valor total do consumo:"))
 pessoas = int(input("Informe o total de pessoas:"))
 servico = int(input("Informe o percentual do serviço, entre 0 e 100:"))
-print ("O valor total da conta, com a taxa de serviço, será de R$",totalConta.replace('.', ','))
-print ("Dividindo a conta por ",pessoas," pessoa(s), cada pessoa deverá pagar R$",totalConta/pessoas)
+print ("O valor total da conta, com a taxa de serviço, será de R$",totalConsumo.replace('.', ','))
+print ("Dividindo a conta por ",pessoas," pessoa(s), cada pessoa deverá pagar R$",contaPessoa)
